1198ac867f1801ce316bb63aaac1f2c60de3dda6
Ahsan Habib Bhuiyan <70793867+ruhit-coder@users.noreply.github.com>
2022-10-07 23:05:05 +0600

    Update and rename index.js to app.js

---
+++ src/scripts/app.js
@@ -0,0 +1,5 @@
+let arr = [1,2,3]
+
+for(let v of arr){
+    console.log(v)
+}
--- src/scripts/index.js
+++
@@ -1,6 +0,0 @@
-import "../styles/style.css";
-
-let nam = "XYZ";
-let age = 22;
-
-console.log("My name is " + nam + ". I'm " + age + " years old.");
