81f376e6c03e4ad04cd82ef5981ab0dc2bcdfa6b
Ahsan Habib Bhuiyan <70793867+ruhit-coder@users.noreply.github.com>
2022-10-07 23:08:07 +0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,21 @@
 const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+require("babel-register");
 
 module.exports = {
   mode: "development",
-  entry: {
-    bundle: path.resolve(__dirname, "src/scripts/index.js"),
-  },
+  entry: ["./src/scripts/app.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
     clean: true,
   },
-  devtool: "inline-source-map",
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    },
-    port: 1234,
-    open: "index.html",
-    compress: true,
-  },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-proposal-class-properties"],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        exclude: /node_modules/,
+        use: ["babel-loader"],
       },
       {
         test: /\.html$/,
@@ -46,15 +24,18 @@ module.exports = {
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: "main.css",
-      chunkFilename: "[id].css",
-    }),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
-      title: "My App",
     }),
-    new BundleAnalyzerPlugin(),
   ],
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 4466,
+    open: "index.html",
+    compress: true,
+  },
 };
